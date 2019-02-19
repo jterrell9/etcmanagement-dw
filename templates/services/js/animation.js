0e4db6e233d2937605d713627ab3cf905e2ff1ea
Jack Terrell <trifeowns@gmail.com>
2019-02-18 23:11:19 -0500

    add more design to services template

--- templates/services/js/animation.js
+++ templates/services/js/animation.js
@@ -14,18 +14,43 @@ $(function(){
 	var services = $('#services');
 	services.delay(5000)
 			.slideDown(500);
+	var copyright = $('#copyright');
+	copyright.delay(5000)
+			.fadeIn(500);
 	
 	var growthCell = $('#growth-cell');
 	var growthIcon = $('#growth-icon');
 	var growthText = $('#growth-text');
 	growthCell.mouseover(function(){
 		growthIcon.attr('src','assets/img/growth-icon_hover.png');
-		growthText.css('color','#FFDA29');
-		growthText.css('text-shadow','1px 1px 1px black');
+		growthText.css('color','#45B5AA');
 	});
 	growthCell.mouseleave(function(){
 		growthIcon.attr('src','assets/img/growth-icon.png');
 		growthText.css('color','black');
-		growthText.css('text-shadow','none');
+	});
+	
+	var graphicDesignCell = $('#graphicDesign-cell');
+	var graphicDesignIcon = $('#graphicDesign-icon');
+	var graphicDesignText = $('#graphicDesign-text');
+	graphicDesignCell.mouseover(function(){
+		graphicDesignIcon.attr('src','assets/img/graphicDesign-icon_hover.png');
+		graphicDesignText.css('color','#45B5AA');
+	});
+	graphicDesignCell.mouseleave(function(){
+		graphicDesignIcon.attr('src','assets/img/graphicDesign-icon.png');
+		graphicDesignText.css('color','black');
+	});
+	
+	var printCell = $('#print-cell');
+	var printIcon = $('#print-icon');
+	var printText = $('#print-text');
+	printCell.mouseover(function(){
+		printIcon.attr('src','assets/img/print-icon_hover.png');
+		printText.css('color','#45B5AA');
+	});
+	printCell.mouseleave(function(){
+		printIcon.attr('src','assets/img/print-icon.png');
+		printText.css('color','black');
 	});
 });
