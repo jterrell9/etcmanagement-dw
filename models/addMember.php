b6a04d3a66bee34a91d257f3d524476348596b41
Jack Terrell <trifeowns@gmail.com>
2019-02-21 14:58:11 -0500

    hide mysql password, continue work on adding to database

--- models/addMember.php
+++ models/addMember.php
@@ -4,72 +4,140 @@
 -->
 
 <?php
-
+//include openSSL encryption and descryption support
 include '../../controllers/php/openSSL.php';
 
+//This function connects to the MySQL database, and then populates a new member record
+//@param: $group_name is artist/company name from registration form
+//@param: $fname is user's first name from registration form
+//@param: $lname is user's last name from registration form
+//@param: $email is user's email from registration form
+//@param: $instagram is user's instagram name from registration form
+//@param: $phone is user's phone from registration form
+//@param: $password is user's password from registration form
 function addMember($group_name,$fname,$lname,$email,$instagram,$phone,$password){
-	$sConnected = false;
+	//connect to MySQL database
+	$isConnected = false;
 	$dsn = 'mysql:host=localhost;dbname=jterrell_etc-users';
 	$dbUsername = 'jterrell_etc';
-	$dbPassword = '6RgVeJQ$(@TL';
-	$dbLink = new PDO($dsn,$dbUsername,$dbPassword);
-	$isConnected = true;
-
-	$accountAlreadyExists = false;
-	
-	$passwordHash = encrypt($password);
-	$data = [
-		'group_name' => $group_name,
-		'fname' => $fname,
-		'lname' => $lname,
-		'email' => $email,
-		'instagram_name' => $instagram,
-		'phone' => $phone,
-		'iv' => $passwordHash['iv'],
-		'hmac' => $passwordHash['hmac'],
-		'hash_password' => $passwordHash['ciphertext'],
-	];
-	
-	
-	$result = $dbLink->query('SELECT * FROM members WHERE email='.$data['email']);
-	
-	if($result->num_rows > 0){
-		$accountAlreadyExists = true;
-//		echo '<script>console.log( "Account for '.$data['email'].' already exists. " );</script>';
-		return;
+	$dbPassword = file_get_contents('/home1/jterrell/etcmanagment-config/.conf/mysql.key')
+	try{
+		$dbLink = new PDO($dsn,$dbUsername,$dbPassword);
+		echo "<script>console.log('MySQL connection successful')</script>";
+		$isConnected = true;
+	}catch(PDOException $e){
+		echo "<script>console.log('".$e->getMessage()."')</script>";
+		$isConnected = false;
 	}
-	echo '<script>console.log( "No account exists for '.$data['email'].'" );</script>';
 	
-	$sql = 'INSERT INTO members (group_name, fname, lname, email) '.
-		'VALUES ('.$data['group_name'].','.$data['fname'].','.$data['lname'].",'".$data['email']."')";
-//	echo '<script>console.log( \''.$sql.'\''' );</script>';
-	$stmt = $dbLink->prepare($sql);
-	$stmt->execute();
-	
-	$sql = 'SELECT member_id FROM members where email="'.$data['email'].'"';
-//	echo '<script>console.log( "'.$sql.'" );</script>';
-	$stmt = $dbLink->prepare($sql);
-	$stmt->execute();
-	$member_id = $stmt->fetch(PDO::FETCH_ASSOC);
-//	echo '<script>console.log( "member_id: '.print_r($member_id).'" );</script>';
-	
-	$sql = 'INSERT INTO instagram (member_id, instagram_name) '.
-		'VALUES ('.$member_id['member_id'].','.$data['instagram_name'].')';
-//	echo '<script>console.log( "'.$sql.'" );</script>';
-	$stmt= $dbLink->prepare($sql);
-	$stmt->execute();
-	
-	$sql = 'INSERT INTO phone (member_id, phone) '.
-		'VALUES ('.$member_id['member_id'].','.$data['phone'].')';
-//	echo '<script>console.log( "'.$sql.'" );</script>';
-	$stmt= $dbLink->prepare($sql);
-	$stmt->execute();
-	
-	$sql = 'INSERT INTO login (member_id, iv, hmac, hash_password) '.
-		'VALUES ('.$member_id['member_id'].','.$data['iv'].','.$data['hmac'].','.$data['hash_password'].')';
-//	echo '<script>console.log( "'.$sql.'" );</script>';
-	$stmt= $dbLink->prepare($sql);
-	$stmt->execute();
-}
+	$accountAlreadyExists = false;
+	if($isConnected){
+		//encrypt password
+		$passwordHash = encrypt($password);
+		echo "<script>console.log(\"password encrypted using openSSL\")</script>"."\r\n";
+		//store input data in array
+		$member = [
+			'group_name' => $group_name,
+			'fname' => $fname,
+			'lname' => $lname,
+			'email' => "'".$email."'",
+			'instagram_name' => $instagram,
+			'phone' => $phone,
+			'iv' => $passwordHash['iv'],
+			'hmac' => $passwordHash['hmac'],
+			'hash_password' => $passwordHash['ciphertext'],
+		];
+		
+		//check databases for a record with the same email
+		$sql = 'SELECT email FROM members WHERE email='.$member['email'];
+		try{
+			$statement = $dbLink->prepare($sql);
+			$statement->execute();
+			echo "<script>console.log(\"".$sql."\")</script>"."\r\n";
+			$result = $statement->fetchColumn(0);
+			$statement->closeCursor();
+			if(!empty($result)){
+				$accountAlreadyExists = true;
+				echo '<script>console.log("Account for '.$member['email'].' already exists.");</script>'."\r\n";
+				return;
+			}
+			echo '<script>console.log("No account exists for '.$member['email'].'");</script>'."\r\n";	
+		}catch(PDOException $e){
+			echo "<script>console.log('".$e->getMessage()."')</script>";
+		}
 
+		//insert registration form data into members table
+		$sql = "INSERT INTO members (account_type, group_name, fname, lname, email) ".
+			"VALUES (2, :group_name, :fname, :lname, :email)";
+		try{
+			$statement = $dbLink->prepare($sql);
+			$statement->bindValue(':group_name', $member['group_name']);
+			$statement->bindValue(':fname', $member['fname']);
+			$statement->bindValue(':lname', $member['lname']);
+			$statement->bindValue(':email', $member['email']);
+			$statement->execute();
+			echo "<script>console.log(\"".$sql."\");</script>";
+		}catch(PDOException $e){
+			echo "<script>console.log('".$e->getMessage()."')</script>";
+		}
+		$member_id = $dbLink->insert_id;
+		//get member_id of current entry
+//		$sql = "SELECT memeber_id FROM members WHERE email=".$member['email'];
+//		try{
+//			$statement = $dbLink->prepare($sql);
+//			$statement->execute();
+//			echo "<script>console.log(\"".$sql."\");</script>";
+//			$result = $statement->fetchColumn(0);
+//			$member_id;
+//			echo "<script>console.log(\""."member_id: ".$member_id."\");</script>";
+//			$statement->closeCursor();
+//		}catch(PDOException $e){
+//			$member_id = null;
+//			echo "<script>console.log('".$e->getMessage()."')</script>";
+//		}
+		
+		//insert instagram name into instagram table
+		$sql = "INSERT INTO instagram (member_id, instagram_name) ".
+			"VALUES (:member_id, :instagram_name)";
+		try{
+			$statement = $dbLink->prepare($sql);
+			$statement->bindValue(':member_id', $member_id);
+			$statement->bindValue(':instagram_name', $member['instagram_name']);
+			$statement->execute();
+			echo "<script>console.log(\"".$sql."\");</script>";
+		}catch(PDOException $e){
+			echo "<script>console.log('".$e->getMessage()."')</script>";
+		}
+		
+		//inert phone into phone table
+		$sql = "INSERT INTO phone (member_id, phone) ".
+			"VALUES (:member_id, :phone)";
+		try{
+			$statement = $dbLink->prepare($sql);
+			$statement->bindValue(':member_id', $member_id);
+			$statement->bindValue(':phone', $member['phone']);
+			$statement->execute();
+			echo "<script>console.log(\"".$sql."\");</script>";
+		}catch(PDOException $e){
+			echo "<script>console.log('".$e->getMessage()."')</script>";
+		}
+		
+		//insert encrypted password data into login table
+		$sql = "INSERT INTO login (member_id, iv, hmac, hash_password) ".
+			"VALUES (:member_id, :iv, :hmac, :hash_password)";
+		try{
+			$statement = $dbLink->prepare($sql);
+			$statement->bindValue(':member_id', $member_id);
+			$statement->bindValue(':iv', $member['iv']);
+			$statement->bindValue(':hmac', $member['hmac']);
+			$statement->bindValue(':hash_password', $member['hash_password']);
+			$statement->execute();
+			echo "<script>console.log(\"".$sql."\");</script>";
+		}catch(PDOException $e){
+			echo "<script>console.log('".$e->getMessage()."')</script>";
+		}
+	}else{
+		echo "<script>console.log('ERROR: not connected to database')</script>";
+	}
+}
 ?>
--- models/mysql-connect.php
+++ models/mysql-connect.php
@@ -3,7 +3,7 @@
 $isConnected = false;
 $dsn = 'mysql:host=localhost;dbname=jterrell_etc-users';
 $dbUsername = 'jterrell_etc';
-$dbPassword = '';
+$dbPassword = file_get_contents('/home1/jterrell/etcmanagment-config/.conf/secret.key');
 try{
 	$dbLink = new PDO($dsn,$dbUsername,$dbPassword);
 	echo "<script>console.log('MySQL connection successful')</script>";
