a00a80c45e1f0db1d9b80c4e8ad4fac8d37d36d0
Jack Terrell <trifeowns@gmail.com>
2019-02-22 14:16:49 -0500

    add sql script for reseting database

---
+++ models/mysql_script/reset-tables.sql
@@ -0,0 +1,8 @@
+DROP TABLE members;
+DROP TABLE login;
+DROP TABLE phone;
+DROP TABLE instagram;
+\. /home1/jterrell/public_html/etcmanagement/models/mysql_script/tables/create-members-table.sql;
+\. /home1/jterrell/public_html/etcmanagement/models/mysql_script/tables/create-login-table.sql;
+\. /home1/jterrell/public_html/etcmanagement/models/mysql_script/tables/create-phone-table.sql;
+\. /home1/jterrell/public_html/etcmanagement/models/mysql_script/tables/create-instagram-table.sql;
