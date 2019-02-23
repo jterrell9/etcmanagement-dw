3a6dcac293e91e89e3c548f5dc2604b60328466c
Jack Terrell <trifeowns@gmail.com>
2019-02-23 09:49:01 -0500

    add copyright

--- controllers/php/landing-form.php
+++ controllers/php/landing-form.php
@@ -1,3 +1,7 @@
+<!--landing page join form processing and validation-->
+<!--Authored by Jack Terrell-->
+<!--Copyright StrongWares Consulting LLC and Etc. Management LLC 2019-->
+
 <?php
 
 $artistName = filter_input(INPUT_POST, 'artist-comp-name');
