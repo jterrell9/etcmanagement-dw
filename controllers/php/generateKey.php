85a1e7ba6fc61267d0d83046f43c7c5287a459df
Jack Terrell <trifeowns@gmail.com>
2019-02-23 09:48:39 -0500

    change variable names to reflect best practices

--- controllers/php/generateKey.php
+++ controllers/php/generateKey.php
@@ -1,7 +1,11 @@
+<!--secret key generator for openSSL encryption-->
+<!--Authored by Jack Terrell-->
+<!--Copyright StrongWares Consulting LLC and Etc. Management LLC 2019-->
+
 <?php
-$File = '';	//put file location of secret.key here
-$Handle = fopen($File, 'w');
+$file = '';	//put file location of secret.key here
+$handle = fopen($file, 'w');
 $key64 = openssl_random_pseudo_bytes(64);
-fwrite($Handle, $key64);
-fclose($Handle);
+fwrite($handle, $key64);
+fclose($handle);
 ?>
