a9772d2eb5547ac945263eb8909daa85ec2dc634
Jack Terrell <trifeowns@gmail.com>
2019-02-24 13:43:31 -0500

    create addArtistsForm.php

---
+++ views/admin/add-artist/addArtistForm.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Add Artist</title>
+</head>
+
+<body>
+	<section id="artist-info-form">
+		<form name=
+	</section>
+</body>
+</html>
