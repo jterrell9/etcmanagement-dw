a258d36373652a95159a043af33aba1237999267
Jack Terrell <trifeowns@gmail.com>
2019-02-27 02:16:14 -0500

    fix php comments

--- controllers/php/login.php
+++ controllers/php/login.php
@@ -1,5 +1,7 @@
-<!--Authored by Jack Terrell-->
-<!--Copyright StrongWares LLC and Etc. Management LLC 2019-->
 <?php
+//Authored by Jack Terrell
+//Copyright StrongWares LLC and Etc. Management LLC 2019
+
+//login form processing and authentication
 
 ?>
