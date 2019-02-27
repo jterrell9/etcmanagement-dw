87d94a0e945d67118568665d12ca8ccb0844c73a
Jack Terrell <trifeowns@gmail.com>
2019-02-27 02:13:07 -0500

    remove empty data bindings

--- controllers/php/landing-form.php
+++ controllers/php/landing-form.php
@@ -6,10 +6,6 @@
 
 $artistName = filter_input(INPUT_POST, 'artist-comp-name');
 $email = filter_input(INPUT_POST, 'email');
-$instagram = '';
-$fname = '';
-$lname = '';
-$phone = '';
 
 $isValid = true;
 
