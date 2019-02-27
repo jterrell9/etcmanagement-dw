cec5d08f1b28ac7c858eaaf0e9696513fc8daf37
Jack Terrell <trifeowns@gmail.com>
2019-02-27 02:14:29 -0500

    fix php comments

--- controllers/php/landing-form.php
+++ controllers/php/landing-form.php
@@ -1,8 +1,8 @@
-<!--landing page join form processing and validation-->
-<!--Authored by Jack Terrell-->
-<!--Copyright StrongWares Consulting LLC and Etc. Management LLC 2019-->
-
 <?php
+//Authored by Jack Terrell
+//Copyright StrongWares Consulting LLC and Etc. Management LLC 2019
+
+//landing page join form processing and validation
 
 $artistName = filter_input(INPUT_POST, 'artist-comp-name');
 $email = filter_input(INPUT_POST, 'email');
