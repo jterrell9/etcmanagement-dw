c1a2c70e612fdd561c0a3616a3cfd59517a6eb70
Jack Terrell <trifeowns@gmail.com>
2019-02-28 14:40:49 -0500

    created a roster_util.php file for functions relevent to maintaining the roster

--- controllers/php/util.php
+++ controllers/php/util.php
@@ -4,30 +4,6 @@
 
 //utitlity functions
 
-//function that scans directory for image files
-//@param $path: path to img directory
-//returns: array of filenames of photo files in path
-function scan_photo_dir($path) {
-	$files = scandir($path);
-	if($files){
-		$photoCount = count($files);
-		$photos = array();
-		for($i=2; $i < $photoCount; $i++) {
-			if(substr(strtolower($files[$i]), -4) === '.jpg' or 
-			   substr(strtolower($files[$i]), -5) === '.jpeg' or 
-			   substr(strtolower($files[$i]), -4) === '.bmp' or 
-			   substr(strtolower($files[$i]), -4) === '.gif' or 
-			   substr(strtolower($files[$i]), -4) === '.ico' or 
-			   substr(strtolower($files[$i]), -4) === '.png' or 
-			   substr(strtolower($files[$i]), -4) === '.svg' or 
-			   substr(strtolower($files[$i]), -5) === '.tiff' or 
-			   substr(strtolower($files[$i]), -4) === '.tif' or
-			   substr(strtolower($files[$i]), -4) === '.pdf') {
-				$photos[] = $files[$i];
-			}
-		}
-	}
-	return $photos;
-}
+
 
 ?>
---
+++ models/artist-roster/roster/php/roster_util.php
@@ -0,0 +1,68 @@
+<?php
+//Authored by Jack Terrell
+//Copyright StrongWares LLC and Etc. Management LLC 2019
+
+//contains functions that provide utilities to work with artist roster
+
+require_once 'Artist_class.php';
+
+//function that scans directory for image files
+//@param $path: path to img directory
+//returns: array of filenames of photo files in path
+function scan_photo_dir($path) {
+	$files = scandir($path);
+	if($files){
+		$photos = array();
+		for($i=2; $i < count($files); $i++) {
+			if(substr(strtolower($files[$i]), -4) === '.jpg' or 
+			   substr(strtolower($files[$i]), -5) === '.jpeg' or 
+			   substr(strtolower($files[$i]), -4) === '.bmp' or 
+			   substr(strtolower($files[$i]), -4) === '.gif' or 
+			   substr(strtolower($files[$i]), -4) === '.ico' or 
+			   substr(strtolower($files[$i]), -4) === '.png' or 
+			   substr(strtolower($files[$i]), -4) === '.svg' or 
+			   substr(strtolower($files[$i]), -5) === '.tiff' or 
+			   substr(strtolower($files[$i]), -4) === '.tif' or
+			   substr(strtolower($files[$i]), -4) === '.pdf') {
+				$photos[] = $files[$i];
+			}
+		}
+	}
+	return $photos;
+}
+
+//function that returns array of .roster files present
+function getRoster_filenames() {
+	$files = scandir('../');
+	if($files) {
+		$roster = array();
+		for($i=2; $i < count($files; i++)) {
+			if(substr(strtolower($files[$i]), -7) === '.roster') {
+				$roster[] = $files[$i];
+			}
+		}
+	}
+}
+
+//function create persistent file representing artists in the roster
+//@pparam $artist: Artist class instance
+//@param $file_name: string containing the filename and path of the roster file to be created
+function persist_artist($artist, $file_name) {
+	if(!($artist instanceof Artist)){
+		throw Exception("Could not serialize artist, incorrect class type.");
+	}
+	$serilized_data = serialize($artist);
+	$roster_file = fopen($file_name, "w");
+	fwrite($roster_file, $serilized_data);
+	fclose($roster_file);
+}
+
+//function deserializes the persisted file in the artist roster and returns an Artist object
+//@param $file_name: string containing the filename and path of the roster file to be deserialized
+function deserialize_artist($file_name) {
+	$persistent_file = fopen($file_name, "r") or die("Unable to open file.");
+	$serialize_data = fread($persistent_file, filesize($file_name));
+	fclose($persistent_file);
+	$artist = unserialize($serialize_data);
+	return $artist;
+}
