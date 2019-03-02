933bfa631b4c2d460c31271df4e1e8e88c5bd38c
Jack Terrell <trifeowns@gmail.com>
2019-03-02 10:23:43 -0500

    add new artist Zubah to roster

---
+++ models/artist-roster/assets/frankie-sinn.php
@@ -0,0 +1,43 @@
+<?php
+//Authored by Jack Terrell
+//Copyright StrongWares LLC and Etc. Management LLC 2019
+
+//this file creates an Artist object for Frankie Sinn, emulating what the admin form will do
+
+require_once '../php/Artist_class.php';
+require_once '../php/roster_util.php';
+
+$name = 'Frankie Sinn';
+$artist_type = array('MC', 'Rapper');
+$genres = array('(host) Dubstep', 'Riddim', 'Heavy Bass', 'Trap');
+$social_media = array(
+	'Facebook' => 'https://www.facebook.com/FrankieSinn/',
+	'Instagram' => 'https://www.instagram.com/followfrankieee/',
+	'YouTube' => 'https://www.youtube.com/frankiesinn',
+	'Soundcloud' => 'https://soundcloud.com/frankiesinn',
+	'Twitter' => 'https://twitter.com/followfrankieee',
+	'Curse Him Hex Him' => 'https://www.facebook.com/FrankieSinn/videos/733367953695866/',
+	'Witch N***A Video' => 'https://youtu.be/e3eqVHyI1lI'
+);
+date_default_timezone_set('America/New_York');
+$bookings = array(
+	new Booking('Slander (MC)', $date = mktime(0, 0, 0, 10, 0, 2018), date('F, Y', $date), 'Buckhead Theater'),
+	new Booking('Ray Vlope (MC)', $date = mktime(0, 0, 0, 11, 0, 2018), date('F, Y', $date), 'Opera Atlanta'),
+	new Booking('Squnto (MC)', $date = mktime(0, 0, 0, 12, 0, 2018), date('F, Y', $date), 'Terminal West'),
+	new Booking('Shaky Beats', $date = mktime(0, 0, 0, 0, 0, 2018), date('Y', $date), 'Piedmont Stage'),
+	new Booking('Going Up', $date = mktime(0, 0, 0, 2, 26, 2019), date('F j, Y', $date), 'Music Room'),
+	new Booking('Iris Presents', $date = mktime(0, 0, 0, 12, 29, 2019), date('l', $date), 'Believe Music Hall', 'Resident MC'),
+	new Booking('404 Day', $date = mktime(0, 0, 0, 4, 4, 2019), date('F j, Y', $date), 'Venue TBA')
+);
+$bio = 'INSERT FRANKIE SINN BIO HERE';
+$press_links = array (
+	'Creative Loafing, 6/16/2016 - Ricky Raw\'s bass music top 5' => "https://creativeloafing.com/content-168415-Ricky-Raw's-bass-music-top-5",
+	'VoyageATL, 2/14/2018 - Meet Roosevelt Council of Vandal Rose' => "http://voyageatl.com/interview/meet-roosevelt-council-vandal-rose-west-atlanta/",
+	'VoyageATL, 4/4/2018 - Meet Frankie Sinn in West Atlanta' => "http://voyageatl.com/interview/meet-frankie-sinn-frankie-sinn-west-atlanta/",
+	'The Masquerade - Frankie Sinn' => "http://www.masqueradeatlanta.com/attraction/frankie-sinn/",
+	'Creative Loafing - Esham, Frankie Sinn at The Masquerade' => "https://local.creativeloafing.com/event-303524-Esham,-Frankie-Sinn"
+);	
+$image_files = scan_photo_dir('../assets/frankie_sinn-img/');
+$frankie_sinn = new Artist($name, $artist_type, $genres, $social_media, $bookings, $bio, $press_links, $image_files);
+update_roster($frankie_sinn);
+?>
---
+++ models/artist-roster/assets/zubah.php
@@ -0,0 +1,40 @@
+<?php
+//Authored by Jack Terrell
+//Copyright StrongWares LLC and Etc. Management LLC 2019
+
+//this file creates an Artist object for Zubah, emulating what the admin form will do
+
+require_once '../php/Artist_class.php';
+require_once '../php/roster_util.php';
+
+$name = 'Zubah';
+$artist_type = array('Producer', 'DJ');
+$genres = array('Dubstep', 'Riddim', 'Heavy Bass', 'Hip Hop');
+$social_media = array(
+	'Soundcloud' => 'https://soundcloud.com/dj-zubah',
+	'Spotify' => 'https://open.spotify.com/artist/2U4IvX2EBvfFfSTTajnxEE',
+	'Facebook' => 'https://www.facebook.com/zubahatl/',
+	'Instagram' => 'https://www.instagram.com/zubahatl/',
+	'Bowser Blockz (31k Plays)' => 'https://soundcloud.com/dj-zubah/zubah-bowserblockz-dub',
+	'Zubah In Montreal' => 'https://www.facebook.com/zubahatl/videos/565511913850271/',
+	'Squnto\'s Mega Chop' => 'https://www.facebook.com/squnto/videos/2166927823521820/',
+	'Space Force EP' => 'https://soundcloud.com/reklaimofficial/sets/spaceforce-ep'
+);
+date_default_timezone_set('America/New_York');
+$bookings = array(
+	new Booking('Headlined Alpha Squadron\'s Bass Show', $date = mktime(0, 0, 0, 1, 0, 2019), date('F, Y', $date), 'Toronto, Canada'),
+	new Booking('Direct Support for Squnto', $date = mktime(0, 0, 0, 12, 0, 2018), date('F, Y', $date), 'Terminal West'),
+	new Booking('Iris Presents', $date = mktime(0, 0, 0, 1, 0, 2019), date('F Y', $date), 'Believe Music Hall'),
+	new Booking('Liquified Presents', $date = mktime(0, 0, 0, 2, 22, 2019), date('F j, Y', $date), 'District'),
+	new Booking('All Originals', $date = mktime(0, 0, 0, 2, 26, 2019), date('F j, Y', $date), 'Music Room')
+);
+$bio = 'INSERT ZUBAH BIO HERE';
+$press_links = array (
+	'The QR Network, 10/22/2018' => "https://www.theqrnetwork.com/interviews/zubah",
+	'The Masquerade - Zubah' => "http://www.masqueradeatlanta.com/attraction/zubah/",
+	'Best Things in Georgia - Squnto and Zubah' => "https://bestthingsga.com/event/squnto-and-zubah-2018-11-08-jb-atlanta-ga.html"
+);	
+$image_files = scan_photo_dir('../assets/zubah-img/');
+$zubah = new Artist($name, $artist_type, $genres, $social_media, $bookings, $bio, $press_links, $image_files);
+update_roster($zubah);
+?>
