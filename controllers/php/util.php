25031faf89a4efdc048bd40c4b5e0def07043864
Jack Terrell <trifeowns@gmail.com>
2019-03-21 21:23:23 -0400

    add controller logic

--- controllers/php/util.php
+++ controllers/php/util.php
@@ -2,8 +2,24 @@
 //Authored By Jack Terrell
 //Copyright StrongWares LLC and Etc. Management LLC 2019
 
+require_once '../../models/artist-roster/php/Artist_class.php';
+require_once '../../models/artist-roster/php/roster_util.php';
+
 //utitlity functions
 
+//this function returns an array of Artists of all the artists in roster
+function getArtists_array() {
+	$rosters = getRoster_filenames();
+	$srtists = array();
+	foreach($rosters as $roster) {
+		$artists[] = deserialize_artist('../../models/artist-roster/assets/'.$roster);
+	}
+	return $artists;
+}
 
+//this function returns the artists' names from the roster
+function getArtists_name() {
+	
+}
 
 ?>
